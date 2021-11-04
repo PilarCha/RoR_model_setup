ff167dc161898f5ec161e0401a768a431f3f5c7f
PilarCha <pchavez92@gmail.com>
2021-11-04 16:13:19 -0700

    pushing before changing projects babyyyy

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.delete_all
+# `ActiveRecord::Base.connection.reset_pk_sequence!('users')`
+
+Restaurant.delete_all
+# `ActiveRecord::Base.connection.reset_pk_sequence!('restaurants')`
+
+Event.delete_all
+# `ActiveRecord::Base.connection.reset_pk_sequence!('events')`
+
+Review.delete_all
+# `ActiveRecord::Base.connection.reset_pk_sequence!('reviews')`
 
 User.create(name: 'Juana la baanana')
 User.create(name: 'Pin pon')
 
 Restaurant.create(name: 'Pizza Hut', address: '1234 Hubba bubba')
-Restaurant.create(name: 'Pizza Hut', address: '1234 Hubba bubba')
+Restaurant.create(name: 'Burger King', address: '444 Burger burger')
+
+Event.create(name: 'EDC');
+Event.create(name: 'Jumping Fun')
+
+# Review.create(content: 'Alot of jumping was had', reviewed_on_id: 20, reviewed_on_type: 'events', User_id: 23)
+
+puts 'All done'
